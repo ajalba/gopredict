f348abd24c08e66ba830e1756a52e819fbf69e11
ajalba <ajalba@correo.ugr.es>
2021-10-29 11:54:42 +0200

    añadido archivo con clase para representar los modelos closes #25

---
+++ src/modelo.py
@@ -0,0 +1,23 @@
+"""
+Clase para representar a los diferentes modelos y su comportamiento
+
+atributos(de momento)
+df=dataframe de entrenamiento proviniente del conjunto de datos de entrenamiento del usuario
+x_train,x_test,y_train,y_test, particiones de df para entrenar el modelo
+
+El resto de métodos son autoexplicativos
+"""
+
+
+class Modelo:
+    #Inicializa un modelo tomando sus datos 
+    def __init__(self,data):
+        pass
+    def realizar_particion():
+        pass
+    def entrenar():
+        pass
+    def predecir():
+        pass
+    def get_metricas_rendimiento():
+        pass
