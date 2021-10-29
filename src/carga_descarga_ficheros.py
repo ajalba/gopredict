7cebe14a7c24c9ec57976b13d2e3921ce3ea5a28
ajalba <ajalba@correo.ugr.es>
2021-10-29 11:49:52 +0200

    creado archivo con funciones para la carga y descarga de ficheros closes #24

---
+++ src/carga_descarga_ficheros.py
@@ -0,0 +1,19 @@
+"""
+Destinado a la carga y descarga de ficheros dentro de la aplicación
+
+"""
+
+"""
+Toma un archivo en formato csv y devuelve un dataframe que se ha hecho con él
+"""
+
+def subida_archivo(nombre_archivo):
+    pass
+
+
+"""
+Toma un data frame y un nombre para el archivo y devuelve un enlace para descargar dicho dataframe
+en formato csv
+"""
+def descarga_archivo(df,nombre_archivo):
+    pass
