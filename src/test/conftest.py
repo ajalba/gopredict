b1c48530a44610b86d95e7ad052d43e2b1d3e19b
ajalba <ajalba@correo.ugr.es>
2021-11-13 20:16:01 +0100

    rehechos test contra logica negocio

--- src/test/conftest.py
+++ src/test/conftest.py
@@ -4,6 +4,7 @@ import pandas as pd
 from .context import gopredict
 from gopredict import Modelo
 
+
 @pytest.fixture(scope='class')
 def modelo_test():
     datos = {'col1': [1, 2], 'col2': [3, 4]}
